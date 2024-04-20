5a3b3cb15f82f1426a66490786751fe392710401
RusokeMarvin <rockymarvin06@gmail.com>
2024-04-20 17:58:13 +0300

    submittofirebase

--- lib/main.dart
+++ lib/main.dart
@@ -10,8 +10,14 @@ import 'package:hello_flutter/pages/patientlist.dart';
 import 'package:hello_flutter/pages/WelcomeScreen.dart';
 import 'package:hello_flutter/pages/prediction.dart';
 import 'package:hello_flutter/pages/predicttb.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
